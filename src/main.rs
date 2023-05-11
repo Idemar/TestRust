72c142606bd002a3611c93a823e37732f8a0acfa
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2023-05-11 20:26:47 +0200

    Test function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused)]
+
+use std::arch::x86_64::_MM_EXCEPT_INEXACT;
+use std::{io, error};
+use std::io::{BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name:");
+    let mut name: String = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Did not recive input");
+
+    println!("Hello {}! {}", name.trim_end(), greeting);
+
 }
